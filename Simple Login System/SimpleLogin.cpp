37c09c50fc882a09d80de8f9b33b740cc5f59214
Tamim-Hasan-Nahian1214 <261452465+Tamim-Hasan-Nahian1214@users.noreply.github.com>
2026-04-10 15:39:51 +0600

    get out dude why do you need my code? go and do it yourself!

--- Simple Login System/SimpleLogin.cpp
+++ Simple Login System/SimpleLogin.cpp
@@ -11,24 +11,32 @@ int main() {
     int input;
     string LoginUsername;
     int LoginPassword;
+    int ChangePassword;
     while (true) {
         std::cout << "Enter Your Input: ";
         std::cin >> input;
-        if (input == 1) {
-            if (!UsernameObserver.expired() && !PasswordObserver.expired()) {
-                cin.ignore(numeric_limits<streamsize>::max(), '\n');
-                std::cout << "Login" << std::endl;
-                std::cout << "Enter Your Username: ";
-                std::getline(std::cin, LoginUsername);
-                std::cout << "Enter Your Password: ";
-                std::cin >> LoginPassword;
-                LoginUsername == *username && LoginPassword == *password ? std::cout << "Correct Password!" << std::endl : std::cout << "Incorrect Username or Password!" << std::endl;
-            }
-            else {
-                std::cout << "This account doesn't exist!" << std::endl;
-            }
-            
+        switch (input) {
+            case 1:
+                if (!UsernameObserver.expired() && !PasswordObserver.expired())
+                {
+                    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+                    std::cout << "Login" << std::endl;
+                    std::cout << "Enter Your Username: ";
+                    std::getline(std::cin, LoginUsername);
+                    std::cout << "Enter Your Password: ";
+                    std::cin >> LoginPassword;
+                    LoginUsername == *username &&LoginPassword == *password ? std::cout << "Correct Password!" << std::endl : std::cout << "Incorrect Username or Password!" << std::endl;
+                }
+                else
+                {
+                    std::cout << "This account doesn't exist!" << std::endl;
+                }
+            case 2: 
+                std::cout << "Change Password" << std::endl;
+                std::cout << "Enter New Password: ";
+                std::cin >> ChangePassword;
         }
+        
     }
     return 0;
 }
